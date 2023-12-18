b2357f6c7fee517d532461e59c88bcdcb96c3244
kangminseo <31132404+kangminseo@users.noreply.github.com>
2023-12-18 18:08:02 +0900

    Update TFTPclient1.py

--- TFTPclient1.py
+++ TFTPclient1.py
@@ -1,13 +1,16 @@
 import socket
 import argparse
-import sys
 from struct import pack
+import time
+import sys
 
 DEFAULT_PORT = 69
 BLOCK_SIZE = 512
 DEFAULT_TRANSFER_MODE = 'octet'
+
 OPCODE = {'RRQ': 1, 'WRQ': 2, 'DATA': 3, 'ACK': 4, 'ERROR': 5}
 MODE = {'netascii': 1, 'octet': 2, 'mail': 3}
+
 ERROR_CODE = {
     0: "Not defined, see error message (if any).",
     1: "File not found.",
@@ -19,113 +22,138 @@ ERROR_CODE = {
     7: "No such user."
 }
 
-class TFTPClient:
-    def __init__(self, server_address):
-        self.server_address = server_address
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.settimeout(10)
 
-    def send_wrq(self, filename, mode):
-        format_str = f'>h{len(filename)}sB{len(mode)}sB'
-        wrq_message = pack(format_str, OPCODE['WRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
-        self.sock.sendto(wrq_message, self.server_address)
+def send_wrq(filename, mode):
+    format_str = f'>h{len(filename)}sB{len(mode)}sB'
+    wrq_message = pack(format_str, OPCODE['WRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
+    sock.sendto(wrq_message, server_address)
+    print(wrq_message)
+    timeout = time.time() + 10  # Timeout set to 10 seconds
+    ack_received = False
 
-    def send_rrq(self, filename, mode):
-        format_str = f'>h{len(filename)}sB{len(mode)}sB'
-        rrq_message = pack(format_str, OPCODE['RRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
-        self.sock.sendto(rrq_message, self.server_address)
+    while True:
+        if time.time() > timeout:
+            print("Timeout: No ACK received.")
+            break
 
-    def send_ack(self, seq_num):
-        format_str = '>hh'
-        ack_message = pack(format_str, OPCODE['ACK'], seq_num)
-        self.sock.sendto(ack_message, self.server_address)
-
-    def send_data(self, seq_num, data):
-        format_str = f'>hh{len(data)}s'
-        data_message = pack(format_str, OPCODE['DATA'], seq_num, data)
-        self.sock.sendto(data_message, self.server_address)
-
-    def receive_file(self, filename):
-        file = open(filename, "wb")
-        seq_number = 0
-
-        while True:
-            data, _ = self.sock.recvfrom(516)
-            opcode = int.from_bytes(data[:2], 'big')
-
-            if opcode == OPCODE['DATA']:
-                seq_number = int.from_bytes(data[2:4], 'big')
-                self.send_ack(seq_number)
-
-                file_block = data[4:]
-                file.write(file_block)
-
-                if len(file_block) < BLOCK_SIZE:
-                    file.close()
-                    break
-
-            elif opcode == OPCODE['ERROR']:
-                error_code = int.from_bytes(data[2:4], byteorder='big')
-                print(ERROR_CODE[error_code])
-                break
-
-            else:
-                break
-
-    def send_file(self, filename):
         try:
-            file_to_send = open(filename, "rb")
+            data, _ = sock.recvfrom(4)
+            opcode = int.from_bytes(data[:2], 'big')
+            ack_block = int.from_bytes(data[2:4], 'big')
+            if opcode == OPCODE['ACK'] and ack_block == 0:  # Expected ACK
+                ack_received = True
+                print("ACK 0 received.")
+                break
+        except socket.error:
+            pass
 
-            while True:
-                data, _ = self.sock.recvfrom(516)
-                opcode = int.from_bytes(data[:2], 'big')
+    if not ack_received:
+        print("No valid response received. Transmission failed.")
+        sys.exit(1)
 
-                if opcode == OPCODE['ACK']:
-                    seq_number = int.from_bytes(data[2:4], 'big') + 1
-                    line = file_to_send.read(512)
 
-                    if not line:
-                        self.send_data(seq_number, b'')
-                        break
+def send_rrq(filename, mode):
+    format_str = f'>h{len(filename)}sB{len(mode)}sB'
+    rrq_message = pack(format_str, OPCODE['RRQ'], bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
+    sock.sendto(rrq_message, server_address)
+    print(rrq_message)
 
-                    self.send_data(seq_number, line)
 
-                    if len(line) < BLOCK_SIZE:
-                        file_to_send.close()
-                        break
+def send_ack(seq_num, server):
+    format_str = '>hh'
+    ack_message = pack(format_str, OPCODE['ACK'], seq_num)
+    sock.sendto(ack_message, server)
+    print(seq_num)
+    print(ack_message)
 
-        except FileNotFoundError:
-            print("파일을 찾을 수 없습니다.")
+
+def send_file(filename):
+    file = open(filename, 'rb')
+    block_number = 1
+
+    while True:
+        file_block = file.read(BLOCK_SIZE)
+        data = pack(f'>hh{len(file_block)}s', OPCODE['DATA'], block_number, file_block)
+        sock.sendto(data, server_address)
+
+        expected_ack = pack('>hh', OPCODE['ACK'], block_number)
+        resend_attempts = 0
+        while resend_attempts < 3:
+            try:
+                ack, _ = sock.recvfrom(4)
+                if ack == expected_ack:
+                    break
+            except socket.timeout:
+                print("Timeout: Resending data.")
+                sock.sendto(data, server_address)
+                resend_attempts += 1
+
+        if resend_attempts == 3:
+            print("Failed to receive ACK. Transmission aborted.")
             sys.exit(1)
 
-    def close(self):
-        self.sock.close()
+        if len(file_block) < BLOCK_SIZE:
+            break
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='TFTP 클라이언트 프로그램')
-    parser.add_argument(dest="host", help="서버 IP 주소", type=str)
-    parser.add_argument(dest="action", help="파일 put 또는 get", type=str)
-    parser.add_argument(dest="filename", help="전송할 파일 이름", type=str)
-    parser.add_argument("-p", "--port", dest="port", action="store", type=int)
-    args = parser.parse_args()
+        block_number += 1
 
-    server_ip = args.host
-    server_port = args.port if args.port is not None else DEFAULT_PORT
-    server_address = (server_ip, server_port)
+    file.close()
+    print("File sent successfully.")
 
-    tftp_client = TFTPClient(server_address)
 
-    mode = DEFAULT_TRANSFER_MODE
-    filename = args.filename
+# parse command line arguments
+parser = argparse.ArgumentParser(description='TFTP client program')
+parser.add_argument(dest="host", help="Server IP address", type=str)
+parser.add_argument(dest="operation", help="get or put a file", type=str)
+parser.add_argument(dest="filename", help="name of file to transfer", type=str)
+parser.add_argument("-p", "--port", dest="port", type=int)
+args = parser.parse_args()
 
-    if args.action == 'get':
-        tftp_client.send_rrq(filename, mode)
-        tftp_client.receive_file(filename)
-        print("전송 성공")
+server_ip = args.host
+server_port = args.port if args.port is not None else DEFAULT_PORT
+server_address = (server_ip, server_port)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(5)  # Set socket timeout to 5 seconds
 
-    elif args.action == 'put':
-        tftp_client.send_wrq(filename, mode)
-        tftp_client.send_file(filename)
-        print("전송 성공")
+mode = DEFAULT_TRANSFER_MODE
+operation = args.operation
+filename = args.filename
 
-    tftp_client.close()
+if operation == 'get':
+    send_rrq(filename, mode)
+    file = open(filename, 'wb')
+    expected_block_number = 1
+
+    while True:
+        data, server_new_socket = sock.recvfrom(516)
+        opcode = int.from_bytes(data[:2], 'big')
+
+        if opcode == OPCODE['DATA']:
+            block_number = int.from_bytes(data[2:4], 'big')
+            if block_number == expected_block_number:
+                send_ack(block_number, server_new_socket)
+                file_block = data[4:]
+                file.write(file_block)
+                expected_block_number += 1
+                print(file_block.decode())
+            else:
+                send_ack(block_number, server_new_socket)
+
+        elif opcode == OPCODE['ERROR']:
+            error_code = int.from_bytes(data[2:4], byteorder='big')
+            print(ERROR_CODE[error_code])
+            break
+
+        else:
+            break
+
+        if len(file_block) < BLOCK_SIZE:
+            file.close()
+            print(len(file_block))
+            break
+
+elif operation == 'put':
+    send_wrq(filename, mode)
+    send_file(filename)
+
+sock.close()
